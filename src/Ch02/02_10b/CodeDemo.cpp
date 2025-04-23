6d267349a4e8ffd85459fd4e0c261177919834a4
Meola-K <med.koeker@gmail.com>
2025-04-23 13:29:06 +0000

    type casting

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -6,7 +6,19 @@
 #include <cstdint>
 
 int main(){
-    
-    std::cout << std::endl << std::endl;
+    float target_x;
+    int32_t sprite_x;
+    uint32_t player_x;
+
+    target_x = -123.45;
+    sprite_x = target_x;
+    player_x = sprite_x;
+
+    std::cout<<"Target (float): "<< target_x<<std::endl;
+    std::cout<<"Sprite (int32_t): "<< sprite_x<<std::endl;
+    std::cout<<"player (uint32_t): "<< static_cast<int>(player_x)<<std::endl;
+
+    std::cout<<std::endl<<std::endl;
+   
     return 0;
 }
