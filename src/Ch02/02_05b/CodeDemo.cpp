24fb03ee974331e2e5069be82a5416f0cbb6a0e7
Meola-K <med.koeker@gmail.com>
2025-04-23 11:48:57 +0000

    Scope Bereich

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -7,8 +7,14 @@ int a, b= 5;
 int main(){
     bool myFlag;
     int a =7;
+    myFlag = false;
     std::cout << "a = " << a << std::endl;
     std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << myFlag << std::endl;
+    std::cout << std::endl << std::endl;
+    myFlag=true;
+    std::cout << "a + b = " << a+b << std::endl;
+    std::cout << "b - a = " << b-a << std::endl;
     std::cout << "flag = " << myFlag << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
