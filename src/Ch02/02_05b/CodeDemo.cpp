6ce125e187ba218d0f117a8d5bff38d856f56b1e
Meola-K <med.koeker@gmail.com>
2025-04-23 11:41:13 +0000

    Globale und lokale Variabel

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -3,9 +3,13 @@
 // Using Variables, by Eduardo Corpeño 
 
 #include <iostream>
-
+int a, b= 5;
 int main(){
-    
+    bool myFlag;
+    int a =7;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << myFlag << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
